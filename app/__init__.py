dd17bf3e2d2adaf7428e0c50b93d2644dbc50909
Leina McDermott <leina05@gmail.com>
2020-12-18 14:10:43 -0500

    add oso to __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -8,12 +8,27 @@ from .fixtures import load_fixture_data
 
 from werkzeug.exceptions import Unauthorized
 
+from oso import Oso
+from sqlalchemy_oso import authorized_sessionmaker, register_models, set_get_session
+from sqlalchemy_oso.roles import enable_roles
+
+
 engine = create_engine("sqlite://")
 Session = sessionmaker(bind=engine)
+
+base_oso = Oso()
+AuthorizedSession = authorized_sessionmaker(
+    bind=engine,
+    get_oso=lambda: base_oso,
+    get_user=lambda: g.current_user,
+    get_action=lambda: g.current_action,
+)
 
 
 def create_app():
     app = Flask(__name__)
+
+    init_oso(app)
 
     Base.metadata.create_all(engine)
 
@@ -27,18 +42,35 @@ def create_app():
     app.register_blueprint(routes.bp)
 
     @app.before_request
-    def set_session_and_user():
+    def set_current_user_and_session():
         if "current_user" not in g:
             email = request.headers.get("user")
             if not email:
                 return Unauthorized("user not found")
             try:
+                # Set basic (non-auth) session for this request
                 basic_session = Session()
                 g.basic_session = basic_session
+
+                # Set user for this request
                 g.current_user = (
                     basic_session.query(User).filter(User.email == email).first()
                 )
+                # Set action for this request
+                actions = {"GET": "READ", "POST": "CREATE"}
+                g.current_action = actions[request.method]
+
+                # Set auth session for this request
+                g.auth_session = AuthorizedSession()
+
             except Exception as e:
                 return Unauthorized("user not found")
 
     return app
+
+
+def init_oso(app):
+    register_models(base_oso, Base)
+    set_get_session(base_oso, lambda: g.basic_session)
+    base_oso.load_file("app/authorization.polar")
+    app.oso = base_oso
