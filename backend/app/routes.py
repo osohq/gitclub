cca3ac151df251f0ac6dae524cec44868a28902d
Gabe Jackson <17556281+gj@users.noreply.github.com>
2021-04-03 23:42:24 -0400

    Comment out unused routes

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -76,48 +76,48 @@ def org_potential_users_index(org_id):
     return jsonify([p.repr() for p in potentials.all()])
 
 
-@bp.route("/orgs/<int:org_id>/repos", methods=["GET"])
-def repos_index(org_id):
-    org = g.basic_session.query(Organization).filter(Organization.id == org_id).first()
-    current_app.oso.authorize(org, actor=g.current_user, action="LIST_REPOS")
-
-    repos = g.auth_session.query(Repository).filter_by(organization=org)
-    return jsonify([repo.repr() for repo in repos])
-
-
-@bp.route("/orgs/<int:org_id>/repos", methods=["POST"])
-def repos_new(org_id):
-    # Create repo
-    repo_name = request.get_json().get("name")
-    org = g.basic_session.query(Organization).filter(Organization.id == org_id).first()
-    repo = Repository(name=repo_name, organization=org)
-
-    # Authorize repo creation + save
-    current_app.oso.authorize(repo, actor=g.current_user, action="CREATE")
-    g.basic_session.add(repo)
-    g.basic_session.commit()
-    breakpoint()  # TODO(gj): how do we get ID of newly created repo?
-    return repo.repr(), 201
+# @bp.route("/orgs/<int:org_id>/repos", methods=["GET"])
+# def repos_index(org_id):
+#     org = g.basic_session.query(Organization).filter(Organization.id == org_id).first()
+#     current_app.oso.authorize(org, actor=g.current_user, action="LIST_REPOS")
+#
+#     repos = g.auth_session.query(Repository).filter_by(organization=org)
+#     return jsonify([repo.repr() for repo in repos])
 
 
-@bp.route("/orgs/<int:org_id>/repos/<int:repo_id>", methods=["GET"])
-def repos_show(org_id, repo_id):
-    # Get repo
-    repo = g.basic_session.query(Repository).filter(Repository.id == repo_id).one()
+# @bp.route("/orgs/<int:org_id>/repos", methods=["POST"])
+# def repos_new(org_id):
+#     # Create repo
+#     repo_name = request.get_json().get("name")
+#     org = g.basic_session.query(Organization).filter(Organization.id == org_id).first()
+#     repo = Repository(name=repo_name, organization=org)
+#
+#     # Authorize repo creation + save
+#     current_app.oso.authorize(repo, actor=g.current_user, action="CREATE")
+#     g.basic_session.add(repo)
+#     g.basic_session.commit()
+#     breakpoint()  # TODO(gj): how do we get ID of newly created repo?
+#     return repo.repr(), 201
 
-    # Authorize repo access
-    current_app.oso.authorize(repo, actor=g.current_user, action="READ")
-    return repo.repr()
+
+# @bp.route("/orgs/<int:org_id>/repos/<int:repo_id>", methods=["GET"])
+# def repos_show(org_id, repo_id):
+#     # Get repo
+#     repo = g.basic_session.query(Repository).filter(Repository.id == repo_id).one()
+#
+#     # Authorize repo access
+#     current_app.oso.authorize(repo, actor=g.current_user, action="READ")
+#     return repo.repr()
 
 
-@bp.route("/orgs/<int:org_id>/repos/<int:repo_id>/issues", methods=["GET"])
-def issues_index(org_id, repo_id):
-    repo = g.basic_session.query(Repository).filter(Repository.id == repo_id).one()
-    current_app.oso.authorize(repo, actor=g.current_user, action="LIST_ISSUES")
-
-    # Get authorized issues
-    issues = g.auth_session.query(Issue).filter(Issue.repository.has(id=repo_id))
-    return jsonify([issue.repr() for issue in issues])
+# @bp.route("/orgs/<int:org_id>/repos/<int:repo_id>/issues", methods=["GET"])
+# def issues_index(org_id, repo_id):
+#     repo = g.basic_session.query(Repository).filter(Repository.id == repo_id).one()
+#     current_app.oso.authorize(repo, actor=g.current_user, action="LIST_ISSUES")
+#
+#     # Get authorized issues
+#     issues = g.auth_session.query(Issue).filter(Issue.repository.has(id=repo_id))
+#     return jsonify([issue.repr() for issue in issues])
 
 
 # # TODO(gj): not currently using 'org_id'
@@ -151,27 +151,27 @@ def issues_index(org_id, repo_id):
 #         return {}, 201
 
 
-@bp.route("/orgs/<int:org_id>/teams", methods=["GET"])
-def teams_index(org_id):
-    org = g.basic_session.query(Organization).filter(Organization.id == org_id).first()
-    current_app.oso.authorize(org, actor=g.current_user, action="LIST_TEAMS")
-
-    teams = g.auth_session.query(Team).filter(Team.organization.has(id=org_id))
-    return jsonify([team.repr() for team in teams])
-
-
-@bp.route("/orgs/<int:org_id>/teams/<int:team_id>", methods=["GET"])
-def teams_show(org_id, team_id):
-    team = g.basic_session.query(Team).get(team_id)
-    current_app.oso.authorize(team, action="READ")
-    return team.repr()
+# @bp.route("/orgs/<int:org_id>/teams", methods=["GET"])
+# def teams_index(org_id):
+#     org = g.basic_session.query(Organization).filter(Organization.id == org_id).first()
+#     current_app.oso.authorize(org, actor=g.current_user, action="LIST_TEAMS")
+#
+#     teams = g.auth_session.query(Team).filter(Team.organization.has(id=org_id))
+#     return jsonify([team.repr() for team in teams])
 
 
-@bp.route("/orgs/<int:org_id>/billing", methods=["GET"])
-def billing_show(org_id):
-    org = g.basic_session.query(Organization).filter(Organization.id == org_id).first()
-    current_app.oso.authorize(org, actor=g.current_user, action="READ_BILLING")
-    return {"billing_address": org.billing_address}
+# @bp.route("/orgs/<int:org_id>/teams/<int:team_id>", methods=["GET"])
+# def teams_show(org_id, team_id):
+#     team = g.basic_session.query(Team).get(team_id)
+#     current_app.oso.authorize(team, action="READ")
+#     return team.repr()
+
+
+# @bp.route("/orgs/<int:org_id>/billing", methods=["GET"])
+# def billing_show(org_id):
+#     org = g.basic_session.query(Organization).filter(Organization.id == org_id).first()
+#     current_app.oso.authorize(org, actor=g.current_user, action="READ_BILLING")
+#     return {"billing_address": org.billing_address}
 
 
 @bp.route("/orgs/<int:org_id>/roles", methods=["GET"])
