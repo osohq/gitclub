012c566e48d9e56015a0100e73b90f5b6ef1d557
Gabe Jackson <17556281+gj@users.noreply.github.com>
2021-04-18 22:35:14 -0400

    Don't render new org link when not logged in

--- frontend/src/views/orgs/Index.tsx
+++ frontend/src/views/orgs/Index.tsx
@@ -1,19 +1,24 @@
-import { useEffect, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { Link, RouteComponentProps } from '@reach/router';
 
 import { Org } from '../../models';
 import { org as orgApi } from '../../api';
+import { UserContext } from '../../App';
 
 export function Index(_: RouteComponentProps) {
+  const user = useContext(UserContext);
   const [orgs, setOrgs] = useState<Org[]>([]);
 
   useEffect(() => {
     orgApi.index().then((os) => setOrgs(os));
   }, []);
 
+  const maybeNewLink =
+    user === 'Guest' ? null : <Link to={`/orgs/new`}>Create new org</Link>;
+
   return (
     <>
-      <Link to="/orgs/new">Create new org</Link>
+      {maybeNewLink}
       <h2>Existing orgs</h2>
       <ul>
         {orgs.map((o) => (
