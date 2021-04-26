3b0651f067047336e53daabc67cbc6f467f70856
Sam Scott <sam@osohq.com>
2021-04-26 16:56:59 -0400

    Use `filter_by`.

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -61,7 +61,7 @@ def create_app(db_path=None, load_fixtures=False):
         if "current_user" not in g:
             if "current_user_id" in flask_session:
                 user_id = flask_session.get("current_user_id")
-                user = session.query(User).filter(id=user_id).one_or_none()
+                user = session.query(User).filter_by(id=user_id).one_or_none()
                 if user is None:
                     flask_session.pop("current_user_id")
                 g.current_user = user
