3c407cf5b47a06c4c5bb9bcc844bb9c35d9fc3fa
Gabe Jackson <17556281+gj@users.noreply.github.com>
2021-05-08 20:35:43 -0400

    Move redirectWithError function out of Notices

--- frontend/src/views/Notices.tsx
+++ frontend/src/views/Notices.tsx
@@ -4,9 +4,16 @@ import { useEffect, useState } from 'react';
 import { FlashNotice, Notice, NoticeContext } from '../components';
 import { View } from '.';
 
-type NoticesProps = RouteComponentProps & { children: JSX.Element[] };
+type Props = RouteComponentProps & { children: JSX.Element[] };
 
-export function Notices({ children, location }: NoticesProps) {
+const redirectWithError = (e?: string) => {
+  const { pathname } = window.location;
+  const error = typeof e === 'string' ? e : `Failed to load ${pathname}`;
+  sessionStorage.setItem('error', error);
+  window.history.back();
+};
+
+export function Notices({ children, location }: Props) {
   const [notices, setNotices] = useState<Map<string, Notice>>(new Map());
 
   const push = (n: Notice) =>
@@ -15,14 +22,6 @@ export function Notices({ children, location }: NoticesProps) {
     setNotices((ns) => new Map([...ns].filter(([k]) => k !== n.type + n.text)));
 
   const error = (text: string) => push({ type: 'error', text });
-
-  const redirectWithError = (e?: string) => {
-    const error =
-      typeof e === 'string' ? e : `Failed to load ${location!.pathname}`;
-    sessionStorage.setItem('error', error);
-    // TODO(gj): navigate(-1) throws errors that I don't feel like debugging.
-    window.history.back();
-  };
 
   useEffect(() => {
     const e = sessionStorage.getItem('error');
