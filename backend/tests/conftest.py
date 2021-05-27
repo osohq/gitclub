226dc44a15a33f95cbdcb848a4660ce0b4c96a67
Gabe Jackson <17556281+gj@users.noreply.github.com>
2021-05-27 10:24:36 -0400

    Align fixture oso with init_oso()

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -1,9 +1,7 @@
 import pytest
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from oso import Oso
-from sqlalchemy_oso import register_models
-from sqlalchemy_oso.roles2 import OsoRoles
+from sqlalchemy_oso import SQLAlchemyOso
 
 from app import create_app, models
 from app.fixtures import load_fixture_data
@@ -20,14 +18,12 @@ def test_app():
 def test_client(test_app):
     from flask.testing import FlaskClient
 
-    test_client = test_app.test_client()
-
     def log_in_as(self, email):
         self.post("/session", json={"email": email})
 
     FlaskClient.log_in_as = log_in_as  # type: ignore
 
-    return test_client
+    return test_app.test_client()
 
 
 @pytest.fixture
@@ -41,18 +37,15 @@ def Session(engine):
 
 
 @pytest.fixture
-def oso_roles(Session):
-    oso = Oso()
-    register_models(oso, models.Base)
-    roles = OsoRoles(oso, models.Base, models.User, Session)
+def oso(Session):
+    oso = SQLAlchemyOso(models.Base)
+    oso.enable_roles(models.User, Session)
     oso.load_file("app/authorization.polar")
-    return oso, roles
+    return oso
 
 
 @pytest.fixture
-def test_db_session(Session, engine, oso_roles):
+def test_db_session(Session, engine, oso):
     models.Base.metadata.create_all(engine)
-    session = Session()
-    _, roles = oso_roles
-    load_fixture_data(session, roles)
-    return session
+    load_fixture_data(engine, oso.roles)
+    return Session()
