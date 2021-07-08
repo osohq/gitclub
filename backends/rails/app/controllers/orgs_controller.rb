f5946c54267bb05f05e93356f3dabafae27ff48a
Graham Kaemmer <gkaemmer@users.noreply.github.com>
2021-07-08 15:04:28 -0400

    Add orgs controller (unauthorized)

---
+++ backends/rails/app/controllers/orgs_controller.rb
@@ -0,0 +1,27 @@
+class OrgsController < ApplicationController
+  def index
+    orgs = Org.all
+    # TODO: authz
+    render json: orgs
+  end
+
+  def create
+    org = Org.new(create_params)
+    # TODO: authz
+    org.save
+    # TODO: assign user role to org
+    render json: org
+  end
+
+  def show
+    org = Org.find(params[:id])
+    # TODO: authz
+    render json: org
+  end
+
+  private
+
+  def create_params
+    params.permit(:name, :base_repo_role, :billing_address)
+  end
+end
--- backends/rails/config/routes.rb
+++ backends/rails/config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   resource :session, only: [:create, :show, :destroy]
+  resources :orgs, only: [:create, :show, :index]
 end
