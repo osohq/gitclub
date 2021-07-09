da4118cd32af32ba0611fe3fe306b9b40711ab9e
Graham Kaemmer <gkaemmer@users.noreply.github.com>
2021-07-09 14:03:12 -0400

    Move cors config to initializer

--- backends/rails/config/application.rb
+++ backends/rails/config/application.rb
@@ -36,16 +36,6 @@ module Gitclub
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
-    
-    config.middleware.insert_before 0, Rack::Cors do
-      allow do
-        origins 'http://localhost:3000'
-        resource '*',
-                 headers: :any,
-                 credentials: true,
-                 methods: [:get, :post, :options, :delete, :put, :patch]
-      end
-    end
 
     config.middleware.use ActionDispatch::Cookies
     config.middleware.use ActionDispatch::Session::CookieStore, key: 'gitclub'
--- backends/rails/config/initializers/cors.rb
+++ backends/rails/config/initializers/cors.rb
@@ -5,12 +5,12 @@
 
 # Read more: https://github.com/cyu/rack-cors
 
-# Rails.application.config.middleware.insert_before 0, Rack::Cors do
-#   allow do
-#     origins 'example.com'
-#
-#     resource '*',
-#       headers: :any,
-#       methods: [:get, :post, :put, :patch, :delete, :options, :head]
-#   end
-# end
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins 'http://localhost:3000'
+    resource '*',
+             headers: :any,
+             credentials: true,
+             methods: [:get, :post, :options, :delete, :put, :patch]
+  end
+end
