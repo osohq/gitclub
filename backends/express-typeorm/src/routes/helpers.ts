3ab3fcbd3b8696c616ac2b7361cf3965fc5ebc7d
Sam Scott <sam@osohq.com>
2021-08-25 17:56:03 -0400

    404 on undefined.

--- backends/express-typeorm/src/routes/helpers.ts
+++ backends/express-typeorm/src/routes/helpers.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from "express";
+import { NotFoundError } from "oso";
 
 export function addRoutes(router, routes) {
     // register express routes from defined application routes
@@ -6,7 +7,7 @@ export function addRoutes(router, routes) {
         (router as any)[route.method](route.route, (req: Request, res: Response, next) => {
             const result = (new (route.controller as any))[route.action](req, res, next);
             if (result instanceof Promise) {
-                result.then(result => result !== null && result !== undefined ? res.send(result) : undefined).catch(next);
+                result.then(result => result !== null && result !== undefined ? res.send(result) : next(new NotFoundError())).catch(next);
             } else if (result !== null && result !== undefined) {
                 res.json(result);
             }
