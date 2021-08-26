8ed5a12e52a2df080fda16861da9e521a44719b2
Gwen Whelan <gwen@osohq.com>
2021-08-26 10:25:03 -0400

    fmt

--- backends/flask-sqlalchemy/app/routes/users.py
+++ backends/flask-sqlalchemy/app/routes/users.py
@@ -12,10 +12,15 @@ def show(user_id):
     user = g.session.get_or_404(User, id=user_id)
     return user.repr()
 
+
 @bp.route("/<int:user_id>/repos", methods=["GET"])
 @session({User: "read_profile", Repo: "read"})
 def index(user_id):
     user = g.session.get_or_404(User, id=user_id)
     # TODO use data filtering!
-    repos = [repo.repr() for repo in g.session.query(Repo) if current_app.oso.is_allowed(user, "read", repo)]
+    repos = [
+        repo.repr()
+        for repo in g.session.query(Repo)
+        if current_app.oso.is_allowed(user, "read", repo)
+    ]
     return jsonify(repos)
